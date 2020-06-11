36aff7dd52392b806963c32622a714de178b85a0
betalantz <betalantz@gmail.com>
2020-06-11 15:41:18 -0700

    creates APIManager, bin exec, and links environmnent

---
+++ lib/ApiManager.rb
@@ -0,0 +1,9 @@
+class Pokedex::APIManager
+
+    BASE_URL = "https://pokeapi.co/api/v2/"
+
+    def self.get_pokemon
+        puts "MAKING A NETWORK REQUEST"
+    end
+
+end
--- lib/environment.rb
+++ lib/environment.rb
@@ -1,5 +1,10 @@
-require "pokedex/version"
+require_relative "./pokedex/version"
+require 'pry'
+require 'httparty'
+
+require_relative './ApiManager'
 
 module Pokedex
+
   # Your code goes here...
 end
